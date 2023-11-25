e10e1906105326a4b7792bf2b117f0b75e20d7a2
osunlaja abiodun <osunlajajamiu97@gmail.com>
2023-11-25 08:28:53 +0100

    auth UI re-design

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Navbar from './components/header/Navbar';
 import { Suspense, lazy } from 'react';
 import { RouteEnum } from './constant/RouteConstant';
 import Loading from './common/Loading';
-import { Container } from '@mui/material';
 
 const HomePage = lazy(() => import('./pages/HomePage'));
 const SigninPage = lazy(() => import('./pages/SigninPage'));
 const SignupPage = lazy(() => import('./pages/SignupPage'));
 const SettingsPage = lazy(() => import('./pages/SettingsPage'));
+const PricingPage = lazy(() => import('./pages/PricingPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
+const PasswordPage = lazy(() => import('./pages/PasswordPage'));
 
 function App() {
   const routes = useRoutes(ROUTES);
 
+  const user = false;
+
   return (
     <Suspense fallback={<Loading />}>
-      <Navbar />
-      <Container className='my-10'>{routes}</Container>
+      {user && <Navbar />}
+      {routes}
     </Suspense>
   );
 }
@@ -28,4 +32,7 @@ const ROUTES = [
   { path: RouteEnum.SIGNIN, element: <SigninPage /> },
   { path: RouteEnum.SIGNUP, element: <SignupPage /> },
   { path: RouteEnum.SETTINGS, element: <SettingsPage /> },
+  { path: RouteEnum.SETTINGS_PASSWORD, element: <PasswordPage /> },
+  { path: RouteEnum.SETTINGS_PROFILE, element: <ProfilePage /> },
+  { path: RouteEnum.SETTINGS_SUBSCRIPTION, element: <PricingPage /> },
 ];
--- src/common/Button.js
+++ src/common/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
   return (
-    <button className='w-full py-2 rounded-lg bg-Primary_main text-Text_main hover:bg-Primary_light'>
+    <button className='w-full py-4 rounded-lg bg-Primary_main text-Text_main hover:bg-Primary_light'>
       {props.children}
     </button>
   );
