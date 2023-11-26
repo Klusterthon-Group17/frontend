bc2bb17ff8adf7e63583fd4d13e4348ed643bb07
osaze12 <osazeagbi@gmail.com>
2023-11-26 03:42:15 +0100

    fix

--- src/common/SignoutModal.js
+++ src/common/SignoutModal.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Dialog from '@mui/material/Dialog';
-import signout from '../assets/svgs/pana.svg';
-import Button from './Button';
+import React from "react";
+import PropTypes from "prop-types";
+import Dialog from "@mui/material/Dialog";
+import signout from "../assets/svgs/pana.svg";
+import Button from "./Button";
 
 const SignoutModal = (props) => {
   const { onClose, selectedValue, open } = props;
@@ -13,23 +13,30 @@ const SignoutModal = (props) => {
 
   return (
     <Dialog onClose={handleClose} open={open}>
-      <div className='p-10 space-y-5'>
+      <div className="p-10 space-y-5">
         <div>
-          <img src={signout} alt='img' className='mx-auto' />
+          <img src={signout} alt="img" className="mx-auto" />
         </div>
 
-        <h1 className='text-center'>
+        <h1 className="text-center">
           Ready to sign out? Confirm for a secure exit.
         </h1>
 
-        <div className='flex gap-5'>
+        <div className="flex gap-5">
           <button
             onClick={handleClose}
-            className='w-full py-4 rounded-lg bg-Text_main hover:bg-red-300 hover:text-Text_main'
+            className="w-full py-4 rounded-lg bg-Text_main hover:bg-red-300 hover:text-Text_main"
           >
             Cancel
           </button>
-          <Button>Confirm</Button>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("health.ai");
+              window.location.href = "/";
+            }}
+          >
+            Confirm
+          </Button>
         </div>
       </div>
     </Dialog>
